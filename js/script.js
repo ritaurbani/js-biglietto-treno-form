075120a480982578c3164754365b6d3dc4395eae
ritaurbani <ritaurbani@gmail.com>
2024-11-08 17:55:58 +0100

    feat: working on eventlistener calculation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//prelevo form, Input
+
+const formElement = document.getElementById("user-form");
+const ageInputElement = document.getElementById("age");
+const kmInputElement = document.getElementById("km");
+
+console.log(formElement,ageInputElement,kmInputElement)
